911e1db93b68925fb27e6a5fee8f54698eefa17e
CiceroLMP <cicerolucasmp@gmail.com>
2026-06-16 14:39:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -59,7 +59,8 @@ const cvButton = document.getElementById("cvButton");
     { img: "assets/html.png", name: "HTML5" },
     { img: "assets/css.png", name: "CSS3" },
     { img: "assets/js.png", name: "JavaScript" },
-    { img: "assets/cSharp.png", name: "C#" },
+    { img: "assets/php.png", name: "PHP" },
+    { img: "assets/laravel.png", name: "Laravel" },
     { img: "assets/mySql.png", name: "SQL" },
   ];
   let start = 0; // índice inicial
